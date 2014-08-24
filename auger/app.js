ade137e4d7dc9bf0c665b7555e2abb18972c8539
augrr <mrvartan@umass.edu>
2014-08-24 16:07:59 -0400

    Second Try

--- auger/app.js
+++ auger/app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'bower_components/bootstrap')));
 
 app.use('/', routes);
 app.use('/users', users);
